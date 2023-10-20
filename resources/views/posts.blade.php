71725dd21eb6b5f3cff9a5c468d6cac3a480b53b
victor braz <victor.90braz@gmail.com>
2023-10-20 12:40:08 +0200

    add css

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,25 @@
 <x-layout>
-    <div class="container">
+    <div class="container" style="display: grid; grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); gap: 20px; border: 2px solid #ccc; padding: 20px;">
         @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{$post->slug}}">
-                        {{$post->title}}
-                    </a>
-                </h1>
-
-                <div>
-                    {!! $post->excerpt !!}
-                </div>
-            </article>
+            <div class="grid-item" style="border: 1px solid #ccc; padding: 10px;">
+                <nav>
+                    <ul style="list-style: none;">
+                        <li style="font-weight: bold; color: #ff3700;">
+                            {{ $post->category->name }}
+                        </li>
+                    </ul>
+                </nav>
+                <article>
+                    <h1 style="font-weight: bold; color: #231713; text-decoration: underline;">
+                        <a href="/posts/{{$post->slug}}" style="text-decoration: none; color: #333;">
+                            {{$post->title}}
+                        </a>
+                    </h1>
+                    <div style="/* Add additional CSS styles here */">
+                        {!! $post->excerpt !!}
+                    </div>
+                </article>
+            </div>
         @endforeach
     </div>
 </x-layout>
-
-
