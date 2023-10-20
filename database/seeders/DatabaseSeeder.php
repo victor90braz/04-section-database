1d857352dec26322e1bd356f8343bfde46651e5a
victor braz <victor.90braz@gmail.com>
2023-10-20 17:37:02 +0200

    create posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,27 +3,59 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void {
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
         $user = User::factory()->create();
 
-        Category::create([
+        $personal = Category::create([
             'name' => 'Personal',
             'slug' => 'personal'
         ]);
 
-        Category::create([
+        $family = Category::create([
             'name' => 'Family',
             'slug' => 'family'
         ]);
 
-        Category::create([
+        $work = Category::create([
             'name' => 'Work',
             'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'Loren ipsum dolar sit amet',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Loren ipsum dolar sit amet',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Loren ipsum dolar sit amet',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         ]);
     }
 }
