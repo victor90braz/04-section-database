479a904c7efa9feafc3744f964b93770dc00fc63
victor braz <victor.90braz@gmail.com>
2023-10-21 13:15:50 +0200

    <a href="/authors/{{ $post->author->id }}">

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
 
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->with('category')->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 
 });
@@ -24,6 +25,14 @@ Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts
+    ]);
+
+});
+
+Route::get('authors/{author}', function (User $author) {
+
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 
 });
